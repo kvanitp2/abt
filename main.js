5f6e11821af1cbde1fd1167fadd08d7273079ef5
kvanitp2 <evgss722@gmail.com>
2023-02-21 12:03:55 +0500

    init

--- main.js
+++ main.js
@@ -38,12 +38,46 @@ function decreaseTime() {
     }
     else {
         let current = --time
-        if (current < 10){
+        if (current < 10) {
             current = `0${current}`
         }
         setTime(current)
     }
 }
-function startGame(){
+function startGame() {
     setInterval(decreaseTime, 1000)
+    createRandomCircle()
+}
+
+board.addEventListener('click', (event)=>{
+    if (event.target.classList.contains('circle')){
+        score++
+        event.target.remove()
+        createRandomCircle()
+    }
+})
+
+function createRandomCircle() {
+    const circle = document.createElement('div')
+    const size = getRandomNumber(10, 60)
+    const { width, height } = board.getBoundingClientRect()
+    const x = getRandomNumber(0, width - size)
+    const y = getRandomNumber(0, height - size)
+
+    circle.classList.add('circle')
+    circle.style.width = `${size}px`
+    circle.style.height = `${size}px`
+    circle.style.top = `${y}px`
+    circle.style.left = `${x}px`
+    circle.style.background = getRandomColor()
+
+    board.append(circle)
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
 }
